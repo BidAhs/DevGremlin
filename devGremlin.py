4b8fd7c436348d30f16013f2e5dd989f2a8bf201
Sebastian Booth <ItzBidAhs@gmail.com>
2025-08-09 10:16:23 -0700

    Add files via upload

--- devGremlin.py
+++ devGremlin.py
@@ -10,9 +10,10 @@ from styles import styles
 import sys
 
 def resource_path(relative_path):
-    try:
+    
+    if hasattr(sys, "_MEIPASS"):
         base_path = sys._MEIPASS
-    except Exception:
+    else:
         base_path = os.path.abspath(".")
 
     return os.path.join(base_path, relative_path)
